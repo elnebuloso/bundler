02725b564547bc28977aa388ff8108ccc2fb0ba9
elnebuloso <jan.thoennessen@googlemail.com>
2014-06-03 21:31:16 +0200

    2.1.0

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 defined('APP_ENV') || define('APP_ENV', (getenv('APP_ENV') ? getenv('APP_ENV') : 'production'));
 
 // error reporting
-error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 'on');
 
