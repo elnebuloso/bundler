8c5e47bf71a90f9b895c948d6266ed5481f9effa
elnebuloso <jan.thoennessen@googlemail.com>
2014-10-14 01:52:00 +0200

    refactored javascript command

---
+++ .bundler/javascript.php
@@ -0,0 +1,14 @@
+<?php return array (
+  'javascriptFoo' => 
+  array (
+    'md5' => 'cf1c4502fb665807b78f2f1ea6619f81',
+    'max' => 'js/javascriptFoo.max.js',
+    'min' => 'js/javascriptFoo.min.js',
+  ),
+  'javascriptBar' => 
+  array (
+    'md5' => '82c15fd14a6a11f4535b57221e79c9c6',
+    'max' => 'js/javascriptBar.max.js',
+    'min' => 'js/javascriptBar.min.js',
+  ),
+);
