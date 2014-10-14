6a63eef8b02fb53f5e0d05637858a86ea3e55feb
elnebuloso <jan.thoennessen@googlemail.com>
2014-10-15 01:03:12 +0200

    refactoring

--- src/Bundler/FileBundler.php
+++ src/Bundler/FileBundler.php
@@ -1,32 +1,20 @@
 <?php
 namespace Bundler;
 
-use Bundler\Config\FileConfig;
-use Bundler\Package\FilePackage;
-use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\Yaml\Yaml;
-
 /**
  * Class FileBundler
  *
  * @author Jeff Tunessen <jeff.tunessen@gmail.com>
  */
-class FileBundler extends AbstractBundler {
+class FileBundler extends AbstractBundler implements Bundler {
 
     /**
-     * @param string $root
-     * @param string $yaml
+     * @var string
      */
-    public function __construct($root, $yaml) {
-        parent::__construct($root, $yaml);
+    protected $title = 'bundling files ...';
 
-        $config = Yaml::parse($yaml);
-        $processor = new Processor();
-        $configuration = new FileConfig();
-        $processedConfiguration = $processor->processConfiguration($configuration, array($config));
-
-        foreach($processedConfiguration['packages'] as $name => $package) {
-            $this->addPackage(FilePackage::createFromArray($this->getRoot(), $name, $package));
-        }
-    }
+    /**
+     * @var string
+     */
+    protected $type = self::TYPE_FILES;
 }
