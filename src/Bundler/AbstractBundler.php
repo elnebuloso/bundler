dbce4e93cc75828a09434b00c4a12d83d388c2b0
elnebuloso <jan.thoennessen@googlemail.com>
2014-10-15 01:03:32 +0200

    refactoring

--- src/Bundler/AbstractBundler.php
+++ src/Bundler/AbstractBundler.php
@@ -1,20 +1,23 @@
 <?php
 namespace Bundler;
 
+use Bundler\Config\FileConfig;
+use Bundler\Config\JavascriptConfig;
+use Bundler\Config\StylesheetConfig;
 use Bundler\Package\Package;
+use Bundler\Package\PackageFactory;
 use Exception;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+use Zend\Log\LoggerInterface;
 
 /**
  * Class AbstractBundler
  *
  * @author Jeff Tunessen <jeff.tunessen@gmail.com>
  */
-class AbstractBundler {
-
-    /**
-     * @var string
-     */
-    private $root;
+abstract class AbstractBundler implements Bundler {
 
     /**
      * @var string
@@ -22,33 +25,92 @@ class AbstractBundler {
     private $yaml;
 
     /**
+     * @var string
+     */
+    private $root;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
+    /**
      * @var Package[]
      */
     private $packages;
 
     /**
-     * @param string $root
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $type;
+
+    /**
      * @param string $yaml
+     * @param string $root
      * @throws Exception
      */
-    function __construct($root, $yaml) {
-        $this->root = realpath($root);
+    public function __construct($yaml, $root = null) {
         $this->yaml = realpath($yaml);
 
-        if($this->root === false) {
-            throw new Exception('invalid root path: ' . $this->root);
+        if($this->yaml === false) {
+            throw new Exception('unable to find configuration: ' . $yaml);
         }
 
-        if($this->yaml === false) {
-            throw new Exception('invalid yaml path: ' . $this->yaml);
+        // no root given from which the files are collected, we take the parent folder from ./.bundler
+        if(empty($root)) {
+            $this->root = dirname(dirname($this->yaml));
+        }
+
+        if(realpath($this->root) === false) {
+            throw new Exception('invalid root path: ' . $this->root);
         }
     }
 
     /**
-     * @return string
+     * @throws Exception
      */
-    public function getRoot() {
-        return $this->root;
+    public function configure() {
+        $config = Yaml::parse($this->getYaml());
+        $processor = new Processor();
+        $configuration = null;
+
+        switch($this->type) {
+            case self::TYPE_FILES:
+                $configuration = new FileConfig();
+                break;
+
+            case self::TYPE_JAVASCRIPT:
+                $configuration = new JavascriptConfig();
+                break;
+
+            case self::TYPE_STYLESHEET:
+                $configuration = new StylesheetConfig();
+                break;
+        }
+
+        if(is_null($configuration)) {
+            throw new Exception('missing configuration to process');
+        }
+
+        $processedConfiguration = $processor->processConfiguration($configuration, array($config));
+
+        $packageFactory = new PackageFactory();
+        $packageFactory->setLogger($this->getLogger());
+        $packageFactory->setOutput($this->getOutput());
+
+        foreach($processedConfiguration['packages'] as $name => $package) {
+            $this->addPackage($packageFactory->create($this->type, $name, $this->getRoot(), $package));
+        }
     }
 
     /**
@@ -59,17 +121,38 @@ class AbstractBundler {
     }
 
     /**
-     * @param Package[] $packages
+     * @return string
      */
-    public function setPackages($packages) {
-        $this->packages = $packages;
+    public function getRoot() {
+        return $this->root;
     }
 
     /**
-     * @return Package[]
+     * @param LoggerInterface $logger
      */
-    public function getPackages() {
-        return $this->packages;
+    public function setLogger($logger = null) {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return LoggerInterface
+     */
+    public function getLogger() {
+        return $this->logger;
+    }
+
+    /**
+     * @param OutputInterface $output
+     */
+    public function setOutput($output = null) {
+        $this->output = $output;
+    }
+
+    /**
+     * @return OutputInterface
+     */
+    public function getOutput() {
+        return $this->output;
     }
 
     /**
@@ -80,10 +163,10 @@ class AbstractBundler {
     }
 
     /**
-     * @param string $name
+     * @return Package[]
      */
-    public function removePackage($name) {
-        unset($this->packages[$name]);
+    public function getPackages() {
+        return $this->packages;
     }
 
     /**
@@ -92,5 +175,46 @@ class AbstractBundler {
      */
     public function getPackageByName($name) {
         return array_key_exists($name, $this->packages) ? $this->packages[$name] : null;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function logInfo($message) {
+        if(!is_null($this->getLogger())) {
+            $this->logger->info($message);
+        }
+        elseif(!is_null($this->getOutput())) {
+            $this->output->writeln("<comment>" . $message . "</comment>");
+        }
+    }
+
+    /**
+     * @param string $message
+     */
+    public function logDebug($message) {
+        if(!is_null($this->getLogger())) {
+            $this->logger->debug($message);
+        }
+        elseif(!is_null($this->getOutput())) {
+            $this->output->writeln("  <info>" . $message . "</info>");
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function bundle() {
+        $this->logInfo($this->title);
+
+        $benchmark = new Benchmark();
+        $benchmark->start();
+
+        foreach($this->getPackages() as $package) {
+            $package->bundle();
+        }
+
+        $benchmark->stop();
+        $this->logInfo("{$this->title} in {$benchmark->getTime()} seconds");
     }
 }
